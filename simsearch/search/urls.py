c4e7dd9633751673a82c9c3b612236f49e8308eb
Lars Yencken <lars@yencken.org>
2010-10-02 18:06:14 +1000

    Adds Http404 and Http500 pages.

--- simsearch/search/urls.py
+++ simsearch/search/urls.py
@@ -15,11 +15,10 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('simsearch.search.views',
     url(r'^$',                      'index',        name='search_index'),
-    url(r'^xhr/$',                  'search_json',  name='search_json'),
-    url(r'^xhr/(?P<pivot>.*)/$',    'search_json',  name='search_json_kanji'),
+#    url(r'^xhr/$',                  'search_json',  name='search_json'),
+#    url(r'^xhr/(?P<pivot>.*)/$',    'search_json',  name='search_json_kanji'),
     url(r'^target/$',               'translate',    name='search_target'),
     url(r'^target/(?P<kanji>.*)/$', 'translate',    name='search_target_kanji'),
-    url(r'^(?P<kanji>.*)/$',        'index',        name='search_index'),
 )
 
 # vim: ts=4 sw=4 sts=4 et tw=78:
